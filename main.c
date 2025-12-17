44972127477b87e9f73a2bd3bfe5b56394821f55
Daemon19 <komanglangendria04@gmail.com>
2025-12-17 15:51:23 +0700

    ❤️‍🩹 Add collision & health system

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "raylib.h"
 #include "raymath.h"
+
+// #define DRAW_HITBOX
 
 #define MAX_PLAYER_BULLETS 3
 #define MAX_POOL_BULLETS (MAX_PLAYER_BULLETS * 2)
@@ -25,6 +28,7 @@ typedef struct {
     bool left_side;
     int bullet_count;
     Texture2D texture;
+    int health;
 } Ship;
 
 typedef struct {
@@ -39,14 +43,29 @@ const int WINDOW_WIDTH = 960;
 const int WINDOW_HEIGHT = 540;
 const int SCREEN_WIDTH = WINDOW_WIDTH / 2;
 const int SCREEN_HEIGHT = WINDOW_HEIGHT / 2;
+
 const int SHIP_WIDTH = 24;
 const int SHIP_HEIGHT = 26;
 const float SHIP_VELOCITY = 3.0f;
+const int SHIP_HITBOX_WIDTH = 14;
+const int SHIP_HITBOX_HEIGHT = 20;
+const int SHIP_INITIAL_HEALTH = 3;
+const int SHIP_HEALTH_X_OFF = 10;
+const int SHIP_HEALTH_Y_OFF = 10;
+
 const int BULLET_WIDTH = 12;
 const int BULLET_HEIGHT = 1;
 const float BULLET_VELOCITY = 10.0f;
 
 RenderTexture2D screen;
+
+Rectangle ShipGetHitbox(const Ship *ship)
+{
+    return (Rectangle){
+        ship->position.x + SHIP_WIDTH / 2.0f - SHIP_HITBOX_WIDTH / 2.0f,
+        ship->position.y + SHIP_HEIGHT / 2.0f - SHIP_HITBOX_HEIGHT / 2.0f,
+        SHIP_HITBOX_WIDTH, SHIP_HITBOX_HEIGHT};
+}
 
 void BulletPoolAddBullet(BulletPool bullet_pool, Ship *owner)
 {
@@ -107,6 +126,28 @@ void BulletPoolDraw(BulletPool bullet_pool)
     }
 }
 
+int BulletPoolHandleCollisions(BulletPool bullet_pool, const Ship *shooter,
+                               const Ship *target)
+{
+    int collision_count = 0;
+    for (int i = 0; i < MAX_POOL_BULLETS; i++) {
+        Bullet *bullet = &bullet_pool[i];
+        if (!bullet->active || bullet->owner != shooter) {
+            continue;
+        }
+
+        Rectangle bullet_rectangle = {bullet->position.x, bullet->position.y,
+                                      BULLET_WIDTH, BULLET_HEIGHT};
+        if (!CheckCollisionRecs(bullet_rectangle, ShipGetHitbox(target))) {
+            continue;
+        }
+
+        BulletDeactivate(bullet);
+        collision_count++;
+    }
+    return collision_count;
+}
+
 void ShipHandleMovement(Ship *ship)
 {
     if (IsKeyDown(ship->key_map.move_up)) {
@@ -147,6 +188,21 @@ void ShipHandleShoot(Ship *ship, BulletPool bullet_pool)
 void ShipDraw(Ship *ship)
 {
     DrawTextureV(ship->texture, ship->position, WHITE);
+
+#ifdef DRAW_HITBOX
+    DrawRectangleLinesEx(ShipGetHitbox(ship), 1.0f, RED);
+#endif // NHITBOX
+}
+
+void ShipDrawHealth(const Ship *ship)
+{
+    char health_str[10];
+    sprintf(health_str, "%d", ship->health);
+    int health_width = MeasureText(health_str, 24);
+    int health_x = ship->left_side
+                       ? SHIP_HEALTH_X_OFF
+                       : SCREEN_WIDTH - health_width - SHIP_HEALTH_X_OFF;
+    DrawText(health_str, health_x, SHIP_HEALTH_Y_OFF, 24, RAYWHITE);
 }
 
 Texture2D ShipLoadTexture(const char *filename, int rotation_degree)
@@ -168,15 +224,20 @@ int main(void)
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Space War");
     SetTargetFPS(60);
 
-    Ship ship1 = {
-        {0, 0}, {0, 0}, {KEY_W, KEY_S, KEY_A, KEY_D, KEY_SPACE},
-        true,   0,      ShipLoadTexture(LEFT_SHIP_TEXTURE_FILEPATH, 90)};
+    Ship ship1 = {{0, 0},
+                  {0, 0},
+                  {KEY_W, KEY_S, KEY_A, KEY_D, KEY_SPACE},
+                  true,
+                  0,
+                  ShipLoadTexture(LEFT_SHIP_TEXTURE_FILEPATH, 90),
+                  SHIP_INITIAL_HEALTH};
     Ship ship2 = {{SCREEN_WIDTH * 0.75f, 0},
                   {0, 0},
                   {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_COMMA},
                   false,
                   0,
-                  ShipLoadTexture(RIGHT_SHIP_TEXTURE_FILEPATH, -90)};
+                  ShipLoadTexture(RIGHT_SHIP_TEXTURE_FILEPATH, -90),
+                  SHIP_INITIAL_HEALTH};
     BulletPool bullet_pool = {0};
     screen = LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
 
@@ -188,11 +249,21 @@ int main(void)
         ShipHandleMovement(&ship2);
         ShipHandleShoot(&ship2, bullet_pool);
 
+        int collision_count =
+            BulletPoolHandleCollisions(bullet_pool, &ship1, &ship2);
+        ship2.health -= collision_count;
+
+        collision_count =
+            BulletPoolHandleCollisions(bullet_pool, &ship2, &ship1);
+        ship1.health -= collision_count;
+
         BeginTextureMode(screen);
         ClearBackground(BLACK);
         BulletPoolDraw(bullet_pool);
         ShipDraw(&ship1);
         ShipDraw(&ship2);
+        ShipDrawHealth(&ship1);
+        ShipDrawHealth(&ship2);
         DrawText("Hello Ray", 100, 100, 24, WHITE);
         EndTextureMode();
 
